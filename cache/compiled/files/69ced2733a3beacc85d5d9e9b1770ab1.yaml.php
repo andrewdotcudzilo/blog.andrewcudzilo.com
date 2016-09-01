f69eca1348d06ca68ca339de7a6afe307b885291
Andrew Cudzilo <andrew.cudzilo@gmail.com>
2016-09-01 00:52:58 +0000

    fix ini grav

---
+++ cache/compiled/files/69ced2733a3beacc85d5d9e9b1770ab1.yaml.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/home/ubuntu/workspace/user/config/security.yaml',
+    'modified' => 1472691077,
+    'data' => [
+        'salt' => 'MjmLD3Wvr9P8GT'
+    ]
+];
---
+++ cache/compiled/files/a2ce350c433385a77c50659080b92e5c.yaml.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/home/ubuntu/workspace/user/plugins/random/random.yaml',
+    'modified' => 1472691064,
+    'data' => [
+        'enabled' => true,
+        'route' => '/random',
+        'redirect' => true,
+        'filters' => [
+            'category' => 'blog'
+        ],
+        'filter_combinator' => 'and'
+    ]
+];
